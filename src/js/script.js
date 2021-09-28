dc4848e910645635dbc9454005efa5b04d0af7ef
Nastia <dolanaval@gmail.com>
2021-09-28 18:16:56 +0300

    catalog

---
+++ src/js/script.js
@@ -0,0 +1,29 @@
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false
+});
+
+document.querySelector('.prev').addEventListener('click', function() {
+    slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function() {
+    slider.goTo('next');
+});
+
+
+
+
+
+    
+$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+    
+
